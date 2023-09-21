94cecbbf369d5a5ff6a9c12b17bdf533831f5203
alexander <alexander.apricot@gmail.com>
2023-09-21 23:45:07 +0200

    Update badges

--- src/functions/snapshot-products/utils/event.utils.spec.ts
+++ src/functions/snapshot-products/utils/event.utils.spec.ts
@@ -1,28 +1,24 @@
-// import { parseBody } from "./event.utils";
+import { parseBody } from "./event.utils";
 
 describe("Event Utils", () => {
 
-  it('should pass', () => {
+  describe("parseBody", () => {
+    it("should be defined", () => {
+      expect(parseBody).toBeDefined();
+    });
 
+    it("should return {} if event body is not present", () => {
+      expect(parseBody({})).toEqual({});
+    });
+
+    it("should return {} if event body is not string", () => {
+      expect(parseBody({ body: null })).toEqual({});
+    });
+
+    it("should return parsed body if event body type is string", () => {
+      expect(parseBody({ body: `{ "hello": "world" }` })).toEqual({
+        hello: "world",
+      });
+    });
   });
-
-  // describe("parseBody", () => {
-  //   it("should be defined", () => {
-  //     expect(parseBody).toBeDefined();
-  //   });
-  //
-  //   it("should return {} if event body is not present", () => {
-  //     expect(parseBody({})).toEqual({});
-  //   });
-  //
-  //   it("should return {} if event body is not string", () => {
-  //     expect(parseBody({ body: null })).toEqual({});
-  //   });
-  //
-  //   it("should return parsed body if event body type is string", () => {
-  //     expect(parseBody({ body: `{ "hello": "world" }` })).toEqual({
-  //       hello: "world",
-  //     });
-  //   });
-  // });
 });
